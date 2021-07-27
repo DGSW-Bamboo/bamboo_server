8e22c64549a53c8b22454abcc12e6427186c2b95
krung2 <jungbin4337@dgsw.hs.kr>
2021-07-27 17:19:29 +0900

    Init: initialize

---
+++ src/main/java/com/bamboo/soda/SodaApplication.java
@@ -0,0 +1,13 @@
+package com.bamboo.soda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SodaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SodaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/bamboo/soda/SodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamboo.soda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SodaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
